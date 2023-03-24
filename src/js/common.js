9844cc41fdf5cb918ce9b1bff8d55c735386d1e4
nineya <lsw@nineya.com>
2023-03-25 02:04:42 +0800

    chore: 适配

--- src/js/common.js
+++ src/js/common.js
@@ -388,8 +388,8 @@ const commonContext = {
   sparkInput() {
     const sparkInputContent = DreamConfig.spark_input_content && DreamConfig.spark_input_content.filter(s => s.length > 0)
     if (sparkInputContent && sparkInputContent.length > 0) {
-      Utils.cachedScript(`${DreamConfig.theme_base}/source/js/spark-input.min.js?mew=${DreamConfig.theme_version}`, function () {
-        $('.spark-input').each((index, domEle) => sparkInput(domEle, sparkInputContent))
+      Utils.cachedScript(`${DreamConfig.theme_base}/js/spark-input.min.js?mew=${DreamConfig.theme_version}`, function () {
+        $('.spark-input').each((index, domEle) => sparkInput(domEle, [domEle.innerText, ...sparkInputContent]))
       })
     }
   },
