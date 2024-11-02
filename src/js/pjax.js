c5fce543ac6061bed59a6dde10732f16f9d0435c
宏尘 <1406993111@qq.com>
2024-11-03 07:58:50 +0800

    fix：修复pjax未能正确排除携带属性 data-not-pjax 的问题
    fix：修复pjax未能正确排除携带属性 data-not-pjax 的问题

--- src/js/pjax.js
+++ src/js/pjax.js
@@ -50,7 +50,7 @@ const syncLoadScripts = ($scripts, i, resolve) => {
  * 第二个参数是容器，即将被替换的内容
  * fragment:是加载的文本中被选中的目标内容
  */
-$(document).on('click', 'a[target!=_blank][href]:not(data-not-pjax)', (event) => {
+$(document).on('click', 'a[target!=_blank][href]:not([data-not-pjax])', (event) => {
   $.pjax.click(event, '.column-main', {
     scrollTo: computeScrollTop(event.currentTarget),
     fragment: '.column-main',
